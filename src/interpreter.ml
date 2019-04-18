fd1682cc547a800d1b922c26cfd12fe0ba8f2a0c
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-04-18 22:56:47 +0200

    Added BINARY_ADD case

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -6,6 +6,7 @@ module H = Hashtbl
 module S = Stack
 
 exception Runtime_error of string
+exception Type_error
 
 type scope = (string, Py_val.t) H.t
 type env = scope list
@@ -46,8 +47,50 @@ let run (c : Bytecode.code) (envr : env) : unit =
                          else s_push (Bool false)
             | Fun _   -> s_push (Bool false)
             | None    -> s_push (Bool true))
-       | UNARY_BW_COMP
-       | BINARY_ADD
+       | UNARY_BW_COMP ->
+           let tos = S.pop stack in
+           (match tos with
+              Int i  -> s_push (Int (lnot i))
+            | Bool b -> if b then s_push (Int (lnot 1))
+                        else s_push (Int (lnot 0))
+            | Float _ | Str _ | Fun _ | None ->
+                raise (Runtime_error "Wrong type: UNARY_BW_COMP"))
+       | BINARY_ADD ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           begin try
+             (match tos1 with
+                Int i1   -> (match tos with
+                               Int i2   -> s_push (Int (i1 + i2))
+                             | Bool b2  -> if b2 then s_push (Int (i1 + 1))
+                                           else s_push (Int i1)
+                             | Float f2 -> s_push (Float (float_of_int i1 +. f2))
+                             | Str _ | Fun _ | None ->
+                                 raise Type_error)
+              | Float f1 -> (match tos with
+                               Int i2   -> s_push (Float (f1 +. float_of_int i2))
+                             | Bool b2  -> if b2 then s_push (Float (f1 +. 1.0))
+                                           else s_push (Float f1)
+                             | Float f2 -> s_push (Float (f1 +. f2))
+                             | Str _ | Fun _ | None ->
+                                 raise Type_error)
+              | Bool b1  -> (match tos with
+                               Int i2   -> if b1 then s_push (Int (i2 + 1))
+                                           else s_push (Int i2)
+                             | Bool b2  -> if b1 && b2 then s_push (Int 2)
+                                           else if not b1 && not b2 then s_push (Int 0)
+                                                else s_push (Int 1)
+                             | Float f2 -> if b1 then s_push (Float (1.0 +. f2))
+                                           else s_push (Float f2)
+                             | Str _ | Fun _ | None ->
+                                 raise Type_error)
+              | Str s1   -> (match tos with
+                               Str s2   -> s_push (Str (s1 ^ s2))
+                             | Int _ | Bool _ | Float _ | Fun _ | None ->
+                                 raise Type_error)
+              | Fun _ | None ->
+                  raise Type_error)
+           with Type_error -> raise (Runtime_error "Type mismatch: BINARY_ADD") end
        | BINARY_SUB
        | BINARY_MULT
        | BINARY_FP_DIV
