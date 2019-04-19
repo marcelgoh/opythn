ddfd447a61bb21ba817c9512e430511e59d7d6e0
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-04-19 23:34:55 +0200

    Added arithmetic operators

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -38,14 +38,22 @@ let is_float = function
 | Float f -> true
 | _       -> false
 
+(* python modulo operator *)
+let rec int_exp x n =
+  if n = 0 then 1
+  else
+    let x' = int_exp x (n / 2) in
+    if n mod 2 = 0 then x' * x'
+    else x * x' * x'
+
 (* run code on virtual stack machine *)
-let run (c : Bytecode.code) (envr : env) : unit =
+let run (c : Bytecode.code) (envr : env) =
   (* stack machine *)
   let (stack : Py_val.t S.t) = S.create () in
   let s_push pv = S.push pv stack in
   (* loop over instructions *)
   let rec loop idx =
-    if idx >= D.length c then ()
+    if idx >= D.length c then stack (* return stack for debugging *)
     else
       let next = ref (idx + 1) in
       (match D.get c idx with
@@ -95,14 +103,59 @@ let run (c : Bytecode.code) (envr : env) : unit =
            let tos1 = S.pop stack in
            (try s_push (Float ((as_float tos1) /. (as_float tos)))
             with Type_error -> raise (Runtime_error "Type mismatch: BINARY_FP_DIV"))
-       | BINARY_INT_DIV
-       | BINARY_MOD
-       | BINARY_EXP
-       | BINARY_LSHIFT
-       | BINARY_RSHIFT
-       | BINARY_BW_AND
-       | BINARY_BW_OR
-       | BINARY_BW_XOR
+       | BINARY_INT_DIV ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try if is_float tos1 || is_float tos then
+                  s_push (Float (floor ((as_float tos1) /. (as_float tos))))
+                else
+                  s_push (Int ((as_int tos1) / (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_INT_DIV"))
+       | BINARY_MOD ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try if is_float tos1 || is_float tos then
+                  (* OPythn modulo differs from in OCaml *)
+                  let r = mod_float (as_float tos1) (as_float tos) in
+                  let ans = if r < 0.0 then r +. (as_float tos) else r in
+                  s_push (Float ans)
+                else let r = (as_int tos1) mod (as_int tos) in
+                     let ans = if r < 0 then r + (as_int tos) else r in
+                     s_push (Int ans)
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_MOD"))
+       | BINARY_EXP ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try if is_float tos1 || is_float tos then
+                  s_push (Float ((as_float tos1) ** (as_float tos)))
+                else s_push (Int (int_exp (as_int tos1) (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_EXP"))
+       | BINARY_LSHIFT ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try s_push (Int ((as_int tos1) lsl (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_LSHIFT"))
+       | BINARY_RSHIFT ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try s_push (Int ((as_int tos1) asr (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_RSHIFT"))
+       | BINARY_BW_AND ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try s_push (Int ((as_int tos1) land (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_BW_AND"))
+       | BINARY_BW_OR ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try s_push (Int ((as_int tos1) lor (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_BW_OR"))
+       | BINARY_BW_XOR ->
+           let tos = S.pop stack in
+           let tos1 = S.pop stack in
+           (try s_push (Int ((as_int tos1) lxor (as_int tos)))
+            with Type_error -> raise (Runtime_error "Type mismatch: BINARY_BW_XOR"))
+       | LOAD_CONST pv -> s_push pv (* temporary position *)
        | COMPARE_EQ
        | COMPARE_NEQ
        | COMPARE_LT
@@ -115,7 +168,6 @@ let run (c : Bytecode.code) (envr : env) : unit =
        | COMPARE_IS_NOT
        | RETURN_VALUE
        | STORE_NAME _
-       | LOAD_CONST _
        | LOAD_NAME _
        | JUMP _
        | POP_JUMP_IF_FALSE _
@@ -134,4 +186,7 @@ let interpret (c : Bytecode.code) : unit =
   H.add built_in_s "print" (Fun Built_in.print);
   let (global_s : scope) = H.create 5 in
   let envr = [global_s; built_in_s] in
-  run c envr
+  let s = run c envr in
+  while not (S.is_empty s) do
+    printf "%s\n" (str_of_py_val (S.pop s));
+  done
