6df7624696d126ca3724df0d3cc25526f8a12b3d
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-04-23 09:40:27 +0200

    Now prints test diffs to output log

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-cd ..
-make
-cd test
+set -e  # abort if any command fails
 
-all_passed="true"
+echo "Running make..."
+cd ..
+make > /dev/null
+cd test
+echo "Compiled OK."
+
+all_passed=1
+
+rm -f "diff_log.txt" # ensure we start with blank log
 
 echo "********** START TESTS **********"
 
-for f in `ls *.opy`
+for f in $(ls *.opy)
 do
-    python3 "$f" > "correct.txt" # use python3 to generate correct output
-    ../main "$f" > "output.txt"
-    if cmp -s "correct.txt" "output.txt"; then  # test output against python3's
+    name=${f%.opy}
+    correct_file="${name}_correct.txt"
+    output_file="${name}_output.txt"
+    python3 "$f" > "$correct_file" # use python3 to generate correct output
+    ../main "$f" > "$output_file"
+    if cmp -s "$correct_file" "$output_file"; then  # test output against python3's
         echo "Passed:" "$f"
+        rm "$correct_file" "$output_file" # remove outputs each time if correct
     else
         echo "Failed:" "$f"
-        echo "Aborting. Check output files."
-        all_passed="false"
-        break
+        diff -u "$correct_file" "$output_file" >> "diff_log.txt"
+        all_passed=0
     fi
-    rm "correct.txt" "output.txt" # remove outputs each time if correct
 done
 
-if [ "$all_passed" = "true" ]; then
-    echo "All tests passed."
-fi
-
 echo "*********** END TESTS ***********"
+
+if (($all_passed)) ; then
+    echo "All tests passed."
+else
+    echo "Some tests failed. See contents of diff_log.txt."
+fi
